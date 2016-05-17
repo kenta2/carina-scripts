0b3caa2f12f235422383169ae7a60d8e85cd9f07
Ken T Takusagawa <kenta@mit.edu>
2016-05-17 13:59:23 -0400

    driver script

---
+++ 00go.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#notes:
+# btrfs is good for thousands and thousands of files
+# 960 540 produces 1920x1080 suitable for HD.
+# input is expected to be a ppm file
+set -x
+if [ -z "$1" ]
+then exit 1
+fi
+nice time carina-scripts/cut04.sh $1 960 540 > log 2>&1
+xz log
